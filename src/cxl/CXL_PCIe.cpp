30d0d47936b7c1e874dc932c00ca9b0477d7704a
SHAO-PENG YANG <es94210@gmail.com>
2022-09-11 15:24:33 -0400

    Ceated simplified host components

--- src/cxl/CXL_PCIe.cpp
+++ src/cxl/CXL_PCIe.cpp
@@ -6,21 +6,44 @@ namespace Host_Components {
 
 	}
 
-	void CXL_PCIe::Start_simulation(){
+	void CXL_PCIe::Deliver(PCIe_Message* message) {
+		returned_request_count++;
+		std::cout << "Returned Request Count: " << returned_request_count << std::endl;
 
 	}
 
-	void CXL_PCIe::Validate_simulation_config() {
+	void CXL_PCIe::Start_simulation(){}
 
-	}
+	void CXL_PCIe::Validate_simulation_config() {}
 	void CXL_PCIe::Execute_simulator_event(MQSimEngine::Sim_Event* event) {
 		Host_IO_Request* io_request{ requests_queue.front()};
 		requests_queue.pop_front();
 
+		PCIe_Message* message{ new PCIe_Message };
+
+		
+		message->Destination = PCIe_Destination_Type::DEVICE;
+		message->Type = PCIe_Message_Type::READ_COMP;
+		message->Address = 0;
+
+		Submission_Queue_Entry* sqe = new Submission_Queue_Entry;
+		sqe->Command_Identifier = 0;
+		sqe->Opcode = (io_request->Type == Host_IO_Request_Type::READ) ? NVME_READ_OPCODE : NVME_WRITE_OPCODE;
+		sqe->Command_specific[0] = (uint32_t)io_request->Start_LBA;
+		sqe->Command_specific[1] = (uint32_t)(io_request->Start_LBA >> 32);
+		sqe->Command_specific[2] = ((uint32_t)((uint16_t)io_request->LBA_count)) & (uint32_t)(0x0000ffff);
+		sqe->PRP_entry_1 = (DATA_MEMORY_REGION);//Dummy addresses, just to emulate data read/write access
+		sqe->PRP_entry_2 = (DATA_MEMORY_REGION + 0x1000);//Dummy addresses
+
+		message->Payload = sqe;
+		message->Payload_size = sizeof(Submission_Queue_Entry);
+
+		pcie_switch->Deliver_to_device(message);
 
 	}
 
-	void CXL_PCIe::Attach_ssd_device(SSD_Components::Host_Interface_CXL* cxl_host_interface) {
-		this->cxl_host_interface = cxl_host_interface;
+	void CXL_PCIe::Set_pcie_switch(PCIe_Switch* pcie_switch) {
+		this->pcie_switch = pcie_switch;
 	}
+
 }
--- src/cxl/CXL_PCIe.h
+++ src/cxl/CXL_PCIe.h
@@ -6,6 +6,7 @@
 #include "../sim/Sim_Object.h"
 #include "../sim/Sim_Event.h"
 #include "../host/Host_IO_Request.h"
+#include "../ssd/Host_Interface_Defs.h"
 #include "Host_Interface_CXL.h"
 
 namespace SSD_Components {
@@ -16,15 +17,17 @@ namespace Host_Components {
 	class CXL_PCIe :public MQSimEngine::Sim_Object {
 	public:
 		CXL_PCIe(const sim_object_id_type& id);
+
+		void Deliver(PCIe_Message* message);
 		void Start_simulation();
 		void Validate_simulation_config();
 		void Execute_simulator_event(MQSimEngine::Sim_Event* event);
-
-		void Attach_ssd_device(SSD_Components::Host_Interface_CXL* cxl_host_interface);
+		void Set_pcie_switch(PCIe_Switch* pcie_switch);
 
 		std::list<Host_IO_Request*> requests_queue;
 	private:
-		SSD_Components::Host_Interface_CXL* cxl_host_interface;
+		PCIe_Switch* pcie_switch;
+		uint64_t returned_request_count{ 0 };
 	};
 }
 
